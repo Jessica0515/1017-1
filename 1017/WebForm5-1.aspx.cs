edb997f143e4d04e7f8ddb763cf5c4e707fff7a1
AA <jessica848688@gmail.com>
2023-10-17 17:00:52 +0800

    1017

---
+++ 1017/WebForm5-1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1017
+{
+    public partial class WebForm5_1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int r = Int32.Parse(Request.QueryString["r"]);
+            int s = Int32.Parse(Request.QueryString["s"]);
+            Label1.Text = (r * s).ToString();
+        }
+    }
+}
---
+++ 1017/WebForm5.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1017
+{
+    public partial class WebForm5 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string r = TextBox1.Text;
+            string s = TextBox2.Text;
+
+            Response.Redirect("WebForm5-1.aspx? r =" + r + "&s=" +s);
+        }
+    }
+}
